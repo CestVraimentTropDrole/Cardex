151e656b8d2cdc2a38bd3389612bcac6cf3930fa
Timéo Pérou <149939786+CestVraimentTropDrole@users.noreply.github.com>
2025-04-23 17:07:42 +0200

    feat: shows card infos + quantity

--- src/Controller/CardController.php
+++ src/Controller/CardController.php
@@ -13,22 +13,37 @@ class CardController extends AbstractController
 {
 
     #[Route('/{extension}-{number}', name: 'card.show', requirements: ['extension' => '[a-z0-9-]+', 'number' => '\d+'])]
-    public function show (Request $request, CardRepository $cardRepository): Response
+    public function show(Request $request, CardRepository $cardRepository): Response
     {
-        $extension = $request->attributes->get('extension');
+        $extension = $request->attributes->get('extension');    // Récupère les infos de la carte dans l'url
         $number = $request->attributes->get('number');
 
-        $card = $cardRepository->findOneBy([
+        $card = $cardRepository->findOneBy([    // Trouve la carte correspondante avec son extension et son numéro
             'number' => $number,
+            'extension' => $extension,
         ]);
 
-        if (!$card) {
+        if (!$card) {   // S'il n'y a aucune carte qui existe dans la db
             throw $this->createNotFoundException('La carte demandée n\'existe pas.');
         }
 
+        $quantity = $this->quantity($card->getQuantity());  // Récupère la quantité de la carte pour recevoir un message formatté
+
         return $this->render('card/card.html.twig', [
             'card' => $card,
+            'quantity' => $quantity,
         ]);
+    }
+    
+    private function quantity(int $quantity) // Renvoie un message formaté en fonction de la quantité possédée de la carte
+    {
+        if ($quantity == 0) {
+            return "Carte non obtenue";
+        } else if ($quantity >= 1) {
+            return "Nombre de carte : " . $quantity;
+        } else {
+            throw $this->createNotFoundException('La valeur entrée n\'est pas valide.');
+        }
     }
 
 }
